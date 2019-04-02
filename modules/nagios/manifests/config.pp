ec6dbbfaeb0ecd8deaa706efc53f68079aca712e
hua <hua@group21mgmt.foo.org.nz>
2019-04-02 20:06:50 +1345

    change config.pp for nagios

--- modules/nagios/manifests/config.pp
+++ modules/nagios/manifests/config.pp
@@ -36,4 +36,23 @@ notification_period => "24x7",
 notification_options => "d,u,r",
 mode => 0444,
 }
+nagios_hostgroup {'ssh-servers':
+target => '/etc/nagios3/conf.d/ppt_hostgroups.cfg',
+alias => 'ssh Servers',
+members => 'group21db.foo.org.nz, group21app.foo.org.nz, group21backups.foo.org.nz, group21mgmt.foo.org.nz',
+}
+nagios_service {'ssh':
+service_description => 'ssh servers',
+hostgroup_name => 'ssh-servers',
+target => '/etc/nagios3/conf.d/ppt_services.cfg',
+check_command => 'check_ssh',
+max_check_attempts => 3,
+retry_check_interval => 1,
+normal_check_interval => 5,
+check_period => '24x7',
+notification_interval => 30,
+notification_period => '24x7',
+notification_options => 'w,u,c',
+contact_groups => 'admins',
+}
 }
